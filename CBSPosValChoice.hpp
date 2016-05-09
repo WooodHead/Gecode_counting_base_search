0bcd8b157c3da32269da1c14b062ef0546ef26e9
Samuel Gagnon <samuel.gagnon92@gmail.com>
2016-05-09 12:26:23 -0400

    Structure varToVal pour accelerer la contrainte all diff
    Il n'y a pas de structure qui est updaté par un Advisor. On crée
    tout simplement la structure varToVal avant chaque choix pour les
    contraintes AllDiff. Ça donne tout de même une accélération
    d'environ 30%.

--- CBSPosValChoice.hpp
+++ CBSPosValChoice.hpp
@@ -1,7 +1,4 @@
-/**
- *  Main author:
- *      Samuel Gagnon
- *
+/*
  *  Permission is hereby granted, free of charge, to any person obtaining
  *  a copy of this software and associated documentation files (the
  *  "Software"), to deal in the Software without restriction, including
--- problems/Sudoku.cpp
+++ problems/Sudoku.cpp
@@ -1,6 +1,6 @@
-/**
- * Based on the Gecode sudoku example by Mikael Lagerkvist, Guido Tack
- * and Christian Schulte.
+/*
+ *  Based on the Gecode sudoku example by Mikael Lagerkvist, Guido Tack
+ *  and Christian Schulte.
  *
  *  Permission is hereby granted, free of charge, to any person obtaining
  *  a copy of this software and associated documentation files (the
